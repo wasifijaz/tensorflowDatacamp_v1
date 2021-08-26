a0274265f3f3176cee0ffbcf348a91328ccd345c
wasifijaz <wasif.ijaz16@yahoo.com>
2021-08-26 16:11:00 +0500

    Package Tracking API:

--- mailroomTrackingWebScraping.py
+++ mailroomTrackingWebScraping.py
@@ -19,11 +19,14 @@ def PressButton(driver,xpath):
     action.perform()
     time.sleep(5)
 
-def SendElement(driver, xpath, data):
+def SendElement(driver,xpath,data):
     element = driver.find_elements_by_xpath(xpath)[0]
     element.send_keys(data)
 
-def Package_Mapping(driver,trackingIds):
+def Package_Mapping(trackingIds):
+    driver = Chrome_Driver()
+    driver.get('https://packagemapping.com/') 
+    time.sleep(10) 
     packageMapping = pd.DataFrame(columns = ['Tracking_ID','Latest_Status','Delivery_Status','Destination','Shipped_From','Shipped_On','Delivered_To','Delivered_On','Service','Shipment_Weight','Signed_By'])
 
     for x in trackingIds:
@@ -43,13 +46,17 @@ def Package_Mapping(driver,trackingIds):
         shipmentWeight = DataByXPath(driver,"//*[@id='us-0']/div[6]/div/div[2]/section/div/div[3]/div[2]/div[2]")
         signedBy = DataByXPath(driver,"//*[@id='us-0']/div[6]/div/div[2]/section/div/div[3]/div[9]/div[2]")
 
-        packageMapping.loc[trackingIds[trackingIds == x].index[0]] = [x,latestStatus,deliveryStatus,destination,shippedFrom,shippedOn,deliveredTo,deliveredOn,service,shipmentWeight,signedBy]
+        packageMapping = packageMapping.append({'Tracking_ID':x,'Latest_Status':latestStatus,'Delivery_Status':deliveryStatus,'Destination':destination,'Shipped_From':shippedFrom,'Shipped_On':shippedOn,'Delivered_To':deliveredTo,'Delivered_On':deliveredOn,'Service':service,'Shipment_Weight':shipmentWeight,'Signed_By':signedBy}, ignore_index=True)
         packageMapping = packageMapping.dropna(thresh = 2, axis = 0)
-    
-    packageMapping.to_csv("D:/packageMapping_Website_Data.csv", index=True)
-    print(packageMapping)
 
-def Package_Trackr(driver,trackingIds):
+    #packageMapping.to_csv("D:/packageMapping_Website_Data.csv", index=True)
+    return packageMapping
+
+def Package_Trackr(trackingIds):
+    driver = Chrome_Driver()
+    driver.get('https://packagetrackr.com/') 
+    time.sleep(10)     
+    
     packageTrackr = pd.DataFrame(columns = ['Tracking_ID','Shipped_From','Shipped_On','Delivered_To','Delivered_On','Service','Shipment_Weight'])
 
     for x in trackingIds:
@@ -58,51 +65,53 @@ def Package_Trackr(driver,trackingIds):
         SendElement(driver,'//*[@id="track-box-form"]/div/input',x)
         PressButton(driver,"//*[@id='track-box-form']/div/span[2]/button")
 
-        shippedFrom = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[8]/div[1]/div[1]/div/div[2]")
-        shippedOn  = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[8]/div[1]/div[2]/div/div[2]")
-        deliveredTo = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[8]/div[2]/div[1]/div/div[2]")
-        deliveredOn = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[8]/div[2]/div[2]/div/div[2]")
-        service = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[8]/div[3]/div[1]/div/div[2]")
-        shipmentWeight = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[8]/div[4]/div[1]/div/div[2]")
+        shippedFrom = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[9]/div[1]/div[1]/div/div[2]")
+        shippedOn  = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[9]/div[1]/div[2]/div/div[2]")
+        deliveredTo = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[9]/div[2]/div[1]/div/div[2]")
+        deliveredOn = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[9]/div[2]/div[2]/div/div[2]")
+        service = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[9]/div[3]/div[1]/div/div[2]")
+        shipmentWeight = DataByXPath(driver,"//*[@id='track-info-panel-region']/div/div[9]/div[4]/div[1]/div/div[2]")
         
-        packageTrackr.loc[trackingIds[trackingIds == x].index[0]] = [x,shippedFrom,shippedOn,deliveredTo,deliveredOn,service,shipmentWeight]
+        packageTrackr = packageTrackr.append({'Tracking_ID':x,'Shipped_From':shippedFrom,'Shipped_On':shippedOn,'Delivered_To':deliveredTo,'Delivered_On':deliveredOn,'Service':service,'Shipment_Weight':shipmentWeight}, ignore_index=True)
         packageTrackr = packageTrackr.dropna(thresh = 2, axis = 0)
 
-        PressButton("//*[@id='body']/div[1]/div/ul/li[1]/a")
+        PressButton(driver,"//*[@id='body']/div[1]/div/ul/li[1]/a")
         if 'google_vignette' in driver.current_url:
             driver.get('https://www.packagetrackr.com/track')
     
-    packageTrackr.to_csv("D:/packageTrackr_Website_Data.csv", index=True)
-    print(packageTrackr)
+   #packageTrackr.to_csv("D:/packageTrackr_Website_Data.csv", index=True)
+    return packageTrackr
+
+def Chrome_Driver():
+    options = webdriver.ChromeOptions()
+    options.binary_location = os.path.normpath("C:/Program Files/Google/Chrome/Application/chrome.exe")
+    chrome_driver_binary = os.path.normpath("C:/Users/wasif.ijaz/Downloads/chromedriver_win32/chromedriver.exe")
+    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+    return driver
+
+def readCSV(file):
+    data = pd.read_csv(file)
+    mailroomData = pd.DataFrame(data)
+
+    trackingIds = mailroomData['tracking_no']
+    trackingIds = trackingIds.dropna(axis=0)
+
+    return trackingIds
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--Package_Mapping',action='store_true')
     parser.add_argument('--Package_Trackr',action='store_true')
     args = parser.parse_args()
-        
-    options = webdriver.ChromeOptions()
-    options.binary_location = os.path.normpath("C:/Program Files/Google/Chrome/Application/chrome.exe")
-    chrome_driver_binary = os.path.normpath("C:/Users/wasif.ijaz/Downloads/chromedriver_win32/chromedriver.exe")
-    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+    
+    file = os.path.normpath("C:/Users/wasif.ijaz/Downloads/Mailroom_OCR_Regex_tracking_Test_Set_Courier_from_DB.csv")
+    trackingIds = readCSV(file)
 
-    data = pd.read_csv(os.path.normpath("C:/Users/wasif.ijaz/Downloads/Mailroom_OCR_Regex_tracking_Test_Set_Courier_from_DB.csv"))
-    mailroomData = pd.DataFrame(data)
-
-    trackingIds = mailroomData['tracking_no']
-    trackingIds = trackingIds.dropna(axis=0)
-
-    if args.Package_Mapping:
-        driver.get('https://packagemapping.com/') 
-        time.sleep(10)     
-        Package_Mapping(driver,trackingIds)
-        driver.close()
+    if args.Package_Mapping:  
+        packagesData = Package_Mapping(trackingIds)
 
     elif args.Package_Trackr:
-        driver.get('https://www.packagetrackr.com/')
-        time.sleep(10)
-        Package_Trackr(driver,trackingIds)
-        driver.close()
+        packagesData = Package_Trackr(trackingIds)
 
 if __name__ == '__main__':
     main()
--- trackingApi.py
+++ trackingApi.py
@@ -1,49 +1,72 @@
-'''
-from flask import request
-from flask.app import Flask
-from flask_restx import Api, Resource, reqparse
-#import mailroomTrackingWebScraping as ws
-
-app = Flask(__tid__)
-#trackingList = []
-
-@app.route('/<tid>/<webtid>')
-def access_param(tid,webtid):
-    #trackingList.append(int(source))
-    print(tid, webtid)
-    return (tid, webtid)
-    
-if __tid__ == '__main__':
-    app.run(debug = True)
-'''
-
 from flask import Flask
 from flask_restx import Api, Resource, reqparse
+from numpy import empty
+from pandas.core.dtypes.missing import notnull
 import mailroomTrackingWebScraping as wc
 import time
+from werkzeug.exceptions import BadRequest
+import pandas as pd
+from werkzeug.datastructures import FileStorage
+import os
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, version='1.0', title='PACKAGE TRACKING API', description='Package tracking using webscrapping techniquues.')
+
+trackingResult = pd.DataFrame()
 
 parser = reqparse.RequestParser()
-parser.add_argument('tid', help='Specify your tid')
-parser.add_argument('webname', help='Specify your website')
+parser.add_argument('Tracking ID', help='Enter your Tracking ID (For multiple enteries use comma):')
+parser.add_argument('Website Name', help='Specify your website:\n\t1. Package Mapping\n\t2. Package Trackr')
 
-@api.route('/trackingID/webName')
+@api.route('/TrackingPackage_UsingTrackingID')
 class TrackingParcel(Resource):
     @api.doc(parser=parser)
     def get(self):        
         args = parser.parse_args()
-        tid = args['tid']
-        webname = args['webname']
-        t_list = [tid]
-        if webname == 'Package Mapping':
-            wc.Package_Mapping(t_list)
-            time.sleep(5)
-        if webname == 'Package Trackr':
-            wc.Package_Trackr(t_list)
-            time.sleep(5)
-        return "This is " + tid + " website: " + webname
-    
+        tid = args['Tracking ID']
+        if not tid:
+            raise BadRequest('Tracking Number Missing!')
+        else:
+            webname = args['Website Name']
+            trackingIds = tid.split(',')
+            if webname == 'Package Mapping' or webname == '1':
+                trackingResult = wc.Package_Mapping(trackingIds)
+                time.sleep(5)
+                return trackingResult.to_json(), 200
+            elif webname == 'Package Trackr' or webname == '2':
+                trackingResult = wc.Package_Trackr(trackingIds)
+                time.sleep(5)
+                return trackingResult.to_json(), 200
+            else:
+                raise BadRequest('Invalid Website Name!')
+
+
+upload_parser = api.parser()
+upload_parser.add_argument('Upload CSV File', location='files', type=FileStorage)
+upload_parser.add_argument('Website Name', help='Specify your website:\n\t1. Package Mapping\n\t2. Package Trackr')
+
+@api.route('/TrackingPackage_UsingCSV')
+@api.expect(upload_parser)
+class UploadFile(Resource):
+    def post(self):
+        args = upload_parser.parse_args()
+        file = args.get('Upload CSV File')
+        split_tup = os.path.splitext(file.filename)
+        if split_tup[1] == '.csv':
+            trackingIds = wc.readCSV(file)
+            webname = upload_parser['Website Name']
+            if webname == 'Package Mapping' or webname == '1':
+                trackingResult = wc.Package_Mapping(trackingIds)
+                time.sleep(5)
+                return trackingResult.to_json(), 200
+            elif webname == 'Package Trackr' or webname == '2':
+                trackingResult = wc.Package_Trackr(trackingIds)
+                time.sleep(5)
+                return trackingResult.to_json(), 200
+            else:
+                raise BadRequest('Invalid Website Name!')
+        else:
+            raise BadRequest('Invalid File Format! Select CSV file.')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
